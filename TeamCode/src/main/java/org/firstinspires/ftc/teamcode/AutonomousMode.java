2c9a25c5f652e0a7d40b46e2a2a34e85adf02d3f
MrKnowname <mrknowname.github@gmail.com>
2017-11-19 20:10:58 -0500

    Added additional files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by William Garland on 11/19/2017.
+ */
+
+@Autonomous(name = "AutonomousMode", group = "Autonomous")
+public class AutonomousMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
