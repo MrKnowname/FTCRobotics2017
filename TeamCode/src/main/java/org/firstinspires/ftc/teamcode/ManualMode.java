5aa0d722ae0fb7bf27d4afef718a8e0ae37b0545
MrKnowname <mrknowname.github@gmail.com>
2017-11-19 19:56:29 -0500

    Added author credits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+/**
+ * Created by William Garland on 11/19/2017.
+ */
+
 @TeleOp(name = "ManualMode", group = "Manual")
 public class ManualMode extends LinearOpMode {
 
